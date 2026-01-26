cf235dc0f7f6069cba594df537dfbfa740191ab7
satokana0418_jp <satokana0418_jp@10.210.236.164>
2026-01-26 16:04:07 +0900

    Question5まで

--- HelloWorld_SatoKanata/src/curriculum/b/Question4.java
+++ HelloWorld_SatoKanata/src/curriculum/b/Question4.java
@@ -9,10 +9,10 @@ public class Question4 {
 		 * Q1
 		 */
 		// int型の配列を作成
-		int[] Num = {1,2,3,4,5};
+		int[] num1 = {1,2,3,4,5};
 		
 		// 格納した値を順番に表示
-		for(int Numbers : Num) {
+		for(int Numbers : num1) {
 			System.out.println(Numbers);
 		}
 		
@@ -21,22 +21,22 @@ public class Question4 {
 		 * Q2
 		 */
 		// 配列を作成
-		int[] Num2 = {10,20,30,40,50};
+		int[] num2 = {10,20,30,40,50};
 		
 		// 末尾から処理する、Num2.length - 1は５０の箇所を指している
-		for(int num = Num2.length - 1; num >= 0; num --){
-			System.out.println(Num2[num]);
+		for(int num = num2.length - 1; num >= 0; num --){
+			System.out.println(num2[num]);
 		}
 		
 		/*
 		 * Q3
 		 */
 		
-		int[] Num3 = {3,5,7,9,11};
+		int[] num3 = {3,5,7,9,11};
 		int total = 0;// 初期化しておく
 		
 		// 取り出した要素を合計する
-		for(int num : Num3) {
+		for(int num : num3) {
 			total += num;
 		}
 		
@@ -45,13 +45,13 @@ public class Question4 {
 		/*
 		 * Q4
 		 */
-		int[] Num4 = {12,7,9,21,5,18};
+		int[] num4 = {12,7,9,21,5,18};
 		// それぞれ０番目の値を基本の値に設定する
-		int high = Num4[0];
-		int low = Num4[0];
+		int high = num4[0];
+		int low = num4[0];
 		
 		// 拡張for文で比較する
-		for(int num : Num4) {
+		for(int num : num4) {
 			if (num > high) {// numの値が現在の値よりも大きかったら更新する
 				high = num;
 			}
@@ -66,16 +66,16 @@ public class Question4 {
 		/*
 		 * Q5
 		 */
-		int[] Num5 = {1,2,3,4,5};
+		int[] num5 = {1,2,3,4,5};
 		
-		for(int num : Num5) {
+		for(int num : num5) {
 			System .out.println(num * 2);// 要素を２倍にして表示
 		}
 		
 		/*
 		 * Q6
 		 */
-		/*int[] Num6 = {4,7,10,15,20};
+		/*int[] num6 = {4,7,10,15,20};
 		
 		Scanner scanner = new Scanner(System.in);
 		System.out.println("入力してください");
@@ -84,7 +84,7 @@ public class Question4 {
 		boolean found = false;
 		
 		// 入力された値が配列の要素に含まれているか判別
-		for(int num : Num6) {
+		for(int num : num6) {
 			if(num == input){
 				found = true;
 				break;
@@ -109,9 +109,9 @@ public class Question4 {
 			{5,6}
 		};
 		// 二重ループ
-		for(int Line1 = 0; Line1 < array.length; Line1++) {// 行(1次元配列)をループ
-			for(int Line2 = 0; Line2 < array[Line1].length; Line2++) {// 列(行の中の要素)をループ
-				System.out.print(array[Line1][Line2] + " ");// printにすることで改行せずに表示
+		for(int line1 = 0; line1 < array.length; line1++) {// 行(1次元配列)をループ
+			for(int line2 = 0; line2 < array[line1].length; line2++) {// 列(行の中の要素)をループ
+				System.out.print(array[line1][line2] + " ");// printにすることで改行せずに表示
 			}
 			System.out.println();
 		}
@@ -128,9 +128,9 @@ public class Question4 {
 		
 		int output = 0;
 		
-		for(int L1 = 0; L1 < array2.length; L1++) {// 行を取り出す
-			for(int L2 = 0; L2 < array2[L1].length; L2++) {// 要素を取り出す
-				output += array2[L1][L2]; // outputに行ごとの要素を足して更新していく
+		for(int l1 = 0; l1 < array2.length; l1++) {// 行を取り出す
+			for(int l2 = 0; l2 < array2[l1].length; l2++) {// 要素を取り出す
+				output += array2[l1][l2]; // outputに行ごとの要素を足して更新していく
 			}
 			System.out.println(output);
 		}
@@ -145,23 +145,23 @@ public class Question4 {
 				{30,2,10}
 		};
 		// 最初の要素で初期化
-		int High = array3[0][0];
-		int Low = array[0][0];
+		int high2 = array3[0][0];
+		int low2 = array[0][0];
 		
 		// 
-		for(int[] L1 : array3) {
-			for(int L2 : L1) {
-				if(L2 > High) {
-					High = L2;// 最大値を更新
+		for(int[] l1 : array3) {
+			for(int l2 : l1) {
+				if(l2 > high2) {
+					high2 = l2;// 最大値を更新
 				}
-				if(L2 < Low) {
-					Low = L2;// 最小値を更新
+				if(l2 < low2) {
+					low2 = l2;// 最小値を更新
 				}
 			}
 		}
 		
-		System.out.println(High);
-		System.out.println(Low);
+		System.out.println(high);
+		System.out.println(low);
 		
 		/*
 		 * Q10
@@ -175,10 +175,10 @@ public class Question4 {
 		};
 		
 		// 3次元配列のため三重ループ
-		for(int L1 = 0; L1 < array4.length; L1++) {
-			for(int L2 = 0; L2 < array[L1].length; L2++) {
-				for(int L3 = 0; L3 < array4[L1][L2].length; L3++) {
-					System.out.print(array4[L1][L2][L3]);
+		for(int l1 = 0; l1 < array4.length; l1++) {
+			for(int l2 = 0; l2 < array[l1].length; l2++) {
+				for(int l3 = 0; l3 < array4[l1][l2].length; l3++) {
+					System.out.print(array4[l1][l2][l3]);
 				}
 			}
 		}
---
+++ HelloWorld_SatoKanata/src/curriculum/b/Question5.java
@@ -0,0 +1,10 @@
+package curriculum.b;
+
+public class Question5 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+}
